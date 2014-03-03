08ebdf0c389f8721ee7821e1859f4870d9b1f093
Tim Jarratt <tjarratt@gmail.com>
2014-03-02 23:05:58 -0800

    Reminder to myself to testdrive more of readline
    Starting to think that this part is really what I want to focus on implementing.
    Arrow keys, ctrl+a, ctrl+e, and delete would all be pretty well. Figuring out
    how to inject dependencies and provide a sane user interface that is also
    testable will be key.

---
+++ wall_street_test.go
@@ -0,0 +1,12 @@
+package wall_street_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Wall Street", func() {
+	It("is not fully implemented", func() {
+		Expect(true).To(BeFalse())
+	})
+})
