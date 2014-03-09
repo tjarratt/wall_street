e64d309c09406a91de28a52767d5eae99dec5218
Tim Jarratt <tjarratt@gmail.com>
2014-03-09 11:11:02 -0700

    Write a better failing test

--- wall_street_test.go
+++ wall_street_test.go
@@ -1,12 +1,30 @@
 package wall_street_test
 
 import (
+	"io"
+	"wall_street"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Wall Street", func() {
-	It("is not fully implemented", func() {
-		Expect(true).To(BeFalse())
+	Describe("Readline", func() {
+
+		It("it reads from stdin and returns a string", func() {
+			simulateSTDIN("The return of the Archons", func(r io.Reader) {
+				readline := wall_street.Readline("Tonight on The Outer Limits")
+				Expect(readline).To(Equal("The return of the Archons"))
+			})
+		})
 	})
 })
+
+func simulateSTDIN(input string, block func(r io.Reader)) {
+	reader, writer := io.Pipe()
+	go func() {
+		defer writer.Close()
+		writer.Write([]byte(input))
+	}()
+
+	block(reader)
+}
